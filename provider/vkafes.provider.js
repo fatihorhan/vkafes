80f998a9cb7a92a2134db477ed9de3dab8ea3995
Fatih Orhan <fatihorhan@gmail.com>
2016-01-23 13:36:37 +0200

    devam

--- provider/vkafes.provider.js
+++ provider/vkafes.provider.js
@@ -9,13 +9,22 @@
     function jsErrorHandlerProvider() {
 		var me = this;
 		
-		me.raise = function(ex, ask_comment){	
+		me.raise = function(ex, scope, ask_comment){	
 			me.exceptionData.exception = ex;
+			me.exceptionData.scope = scope;
 			if(ask_comment == null)
 				ask_comment = me.config.ask_comment;
 			if(ask_comment==true)
 				me.exceptionData.user_comment = prompt("Hata oluştu ve gönderilecek. Eklemek istedğiniz notunuz var mı?");
 			console.log(me.exceptionData);
+		}
+		
+		me.guard = function(method){
+			try{
+				method();
+			} catch(e){
+				me.raise(e);
+			}
 		}
 		
 		me.exceptionData = {
