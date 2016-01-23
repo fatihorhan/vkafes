37e19e6144bfe411b814a9ba1c14c46d66cb3c1f
Kadri Karagöz <junkadri@hotmail.com>
2016-01-23 17:56:59 +0200

    bug fix

--- api/node/api.js
+++ api/node/api.js
@@ -13,11 +13,14 @@ var exists = fs.existsSync(file);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
 
-db.serialize(function() {
+function dbOpen(){
+  db.serialize(function() {
   if(!exists) {
     db.run("CREATE TABLE Errors (errortime TEXT, body TEXT)");
   }
 });
+}
+
 //end DB operation
 
 app.use(express.static(__dirname + '/../../'));
@@ -38,25 +41,27 @@ app.post('/saveerror', function(req, res){
 	console.log("Message : " + ev.exception.message);
 	console.log("Stack Trace : " + ev.exception.stack);
 
+  dbOpen();
 	var stmt = db.prepare("INSERT INTO Errors VALUES (?, ?)");
   	
     stmt.run((new Date()).toString(), JSON.stringify(req.body, null, 2));
   	
   	stmt.finalize();
-
 });
 
 app.get('/errorlist', function(req, res){
 	res.setHeader('Content-Type', 'application/json');
 	res.write('[');
-	db.each("SELECT rowid, errortime, body FROM Errors ORDER BY date(errortime) DESC ", 
+  dbOpen();
+  var i = 0 ;
+	db.each("SELECT rowid, errortime, body FROM Errors ORDER BY rowid DESC ", 
 		function(err, row) {
-			if(row.rowid > 1) res.write(',');
-			res.write('{errortime: "'+row.errortime+'", body: "'+row.body+'"}');
+			if( (i++) > 0) res.write(',');
+			res.write('{rowid: '+row.rowid+', errortime: "'+row.errortime+'", body: "'+row.body+'"}');
   	}, function(){
   		res.end(']');
   	});
-  	
 });
+
 
 app.listen(4444); 
